27dc944d9c08f91f2e0762372a5ff32d3d5b6a62
Ruhollah Nasim <ruhollah123@hotmail.com>
2019-12-25 19:43:42 -0500

    complete 21.

---
+++ MOOC II/21.Printer/com/Printer/Run_/Printer.java
@@ -0,0 +1,35 @@
+package com.Printer.Run_;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Printer {
+
+	private File file;
+	private Scanner reader;
+
+	public Printer(String fileName) throws Exception {
+		this.file = new File(fileName);
+	}
+
+	public String getFilePath() {
+		return file.getAbsolutePath();
+	}
+
+	public void printLinesWhichContain(String word) throws FileNotFoundException {
+		reader = new Scanner(file);
+		if (word.equals("")) {
+			while (reader.hasNextLine()) {
+				System.out.println(reader.nextLine());
+			}
+		} else {
+			while (reader.hasNextLine()) {
+				String line = reader.nextLine();
+				if (line.contains(word)) {
+					System.out.println(line);
+				}
+			}
+		}
+	}
+}
---
+++ MOOC II/21.Printer/com/Printer/Run_/Run_.java
@@ -0,0 +1,17 @@
+package com.Printer.Run_;
+
+public class Run_ {
+
+	public static void main(String[] args) throws Exception {
+
+		Printer printer = new Printer("21.Printer\\textfile.txt");
+
+		printer.printLinesWhichContain("Väinämöinen");
+		System.out.println("-----");
+		printer.printLinesWhichContain("Frank Zappa");
+		System.out.println("-----");
+		printer.printLinesWhichContain("");
+		System.out.println("-----");
+	}
+
+}
